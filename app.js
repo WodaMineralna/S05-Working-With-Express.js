f32d02d677f1aba8766e1cfbdaa932aefb897326
WodaMineralna <raknieborak65@gmail.com>
2025-07-09 21:06:18 +0200

    Setting up two Middlewares as an example, shortifying the server creation
    - Using Express.js

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-const http = require("http");
+const express = require("express");
 
-const server = http.createServer();
+const app = express();
 
-server.listen(3000);
+app.use((req, res, next) => {
+  console.log("In the middleware!");
+  next(); // ! This allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+  console.log("In another middleware!");
+
+  // * Default Response Header: "text/html"
+  res.send("<h1>Hello from Express.js!</h1>");
+});
+
+console.log("Outside middleware");
+
+app.listen(3000);
