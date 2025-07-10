e0bf959bec392da74fe5768ac36fc657ad4cef93
WodaMineralna <raknieborak65@gmail.com>
2025-07-10 18:03:54 +0200

    Using a Helper Function for Navigation
    - So we don't have to think about if we should include [...]
      -> [...] ".." in our path.join(), when sending a File to the client

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,8 @@ const path = require("path");
 
 const express = require("express");
 
+const rootDir = require("../util/path");
+
 const router = express.Router();
 
 // ^ /admin/add-product => GET
@@ -9,7 +11,7 @@ router.get("/add-product", (req, res, next) => {
   console.log("In /add-product middleware!");
 
   // * Default Response Header: "text/html"
-  res.sendFile(path.join(__dirname, "..", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 // ^ /admin/add-product => POST
